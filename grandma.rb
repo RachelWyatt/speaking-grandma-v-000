9e4ad219eab0dba64932bf040124f548923a8f7f
Rachel Wyatt <rw126@evansville.edu>
2018-08-31 15:18:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 
 
 def speak_to_grandma(phrase)
-  phrase = "I LOVE YOU GRANDMA!"
+  phrase = gets.chomp
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == input.upcase
